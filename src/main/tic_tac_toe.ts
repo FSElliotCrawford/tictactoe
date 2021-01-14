7656d5d4a265df0adb9dcbca9973e3ddaef46655
Elliot Crawford <elliot@focalstrategy.com>
2021-01-14 09:43:56 +0000

    Add new tests

--- src/main/tic_tac_toe.ts
+++ src/main/tic_tac_toe.ts
@@ -1,8 +1,24 @@
-export class TicTacToe {
-    play(player: string, pos_x: number, pos_y: number) {
-        if (player === "O"){
+type LastPlayer = "X" | "O" | null;
+
+interface TicTacToeInterface {
+    last_player: LastPlayer
+}
+
+export class TicTacToe implements TicTacToeInterface {
+
+    last_player : LastPlayer = null
+
+    play(player : LastPlayer, pos_x: number, pos_y: number) {
+
+        if(!this.last_player && player === "O") {
             return false;
         }
+
+        if(this.last_player === player) {
+            return false;
+        }
+
+        this.last_player = player;
         return true;
     }
 }
--- src/test/tic_tac_toe.test.ts
+++ src/test/tic_tac_toe.test.ts
@@ -14,4 +14,16 @@ describe('Tic Tac Toe Test', () => {
         let handler: TicTacToe = new TicTacToe();
         expect(handler.play("O", 1, 0)).toBe(false);
     })
+
+    it("If X plays twice the it should fail", () => {
+        let handler: TicTacToe = new TicTacToe();
+        expect(handler.play("X", 1, 0)).toBe(true);
+        expect(handler.play("X", 2, 0)).toBe(false);
+    })
+
+    it("X first, then O", () => {
+        let handler: TicTacToe = new TicTacToe();
+        expect(handler.play("X", 1, 0)).toBe(true);
+        expect(handler.play("O", 2, 0)).toBe(true);
+    })
 })
