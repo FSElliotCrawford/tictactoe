6c1c79d83f4b60dc9026f3857b2ce7e1caa69566
Elliot Crawford <elliot@focalstrategy.com>
2021-01-14 09:28:06 +0000

    Add test and handler

---
+++ src/main/tic_tac_toe.ts
@@ -0,0 +1,5 @@
+export class TicTacToe {
+    play(player: string, pos_x: number, pos_y: number) {
+        return true;
+    }
+}
---
+++ src/test/tic_tac_toe.test.ts
@@ -0,0 +1,13 @@
+import {TicTacToe} from "../main/tic_tac_toe";
+
+
+/*
+ tictactoe.play("O",1,0)
+*/
+
+describe('Tic Tac Toe Test', () => {
+    it("X must be first", () => {
+        let handler: TicTacToe = new TicTacToe();
+        expect(handler.play("X", 1, 0)).toBe(true);
+    })
+})
